c171d2e45205d31678d1d905e8c1d883029c4849
cgivre <cgivre@gmail.com>
2017-08-25 13:23:48 -0400

    Added tons of functionality

--- app2.js
+++ app2.js
@@ -1,6 +1,19 @@
 var Drill = require('./drill.js');
 var client = new Drill.Client({hostname:'localhost', port:8047});
 
-client.query('SELECT * FROM cp.`employee.json` LIMIT 10', function(error, data, columns){
+/*client.query('SELECT * FROM cp.`employee.json` LIMIT 10', function(error, data, columns){
   console.log({data: data['rows'], columns: data['columns'], error: error});
+});*/
+
+client.cluster( function( error, response, body ){
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
 });
+
+
+
+
+/*client.plan('SELECT * FROM cp.`employee.json` LIMIT 10', function(error, data, columns){
+  console.log({data: data['rows'], columns: data['columns'], error: error});
+});*/
--- drill.js
+++ drill.js
@@ -41,9 +41,9 @@ var Client = exports.Client = function(args){
 Client.prototype.execute = function(queryString, callback) {
   var href = url.format({
     protocol: this.protocol,
-    hostname: 'localhost',
+    hostname: this.host,
     pathname: '/query.json',
-    port: 8047
+    port: this.port
   })
   var headers = {
     'Content-Type': 'application/json; charset=UTF-8',
@@ -73,7 +73,7 @@ Client.prototype.query = function(queryString, callback) {
     protocol: this.protocol,
     hostname: 'localhost',
     pathname: '/query.json',
-    port: 8047
+    port: this.port
   })
   var headers = {
     'Content-Type': 'application/json; charset=UTF-8',
@@ -87,8 +87,272 @@ Client.prototype.query = function(queryString, callback) {
   }
   request(queryOptions, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log('victory')
       callback(null, body);
     } //TODO Add error handling
   });
 };
+
+Client.prototype.query = function(queryString, callback) {
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/query.json',
+    port: this.port
+  })
+  var headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Accept':'application/json'
+  }
+  var queryOptions = {
+    uri: href,
+    method: 'POST',
+    headers: headers,
+    json: {"queryType": "SQL", "query": queryString}
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, body);
+    } //TODO Add error handling
+  });
+};
+
+Client.prototype.plan = function(queryString, callback) {
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/query.json',
+    port: this.port
+  })
+  var headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Accept':'application/json'
+  }
+  var queryOptions = {
+    uri: href,
+    method: 'POST',
+    headers: headers,
+    json: {"queryType": "SQL", "query": "EXPLAIN PLAN FOR " + queryString}
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, body);
+    } //TODO Add error handling
+  });
+};
+
+Client.prototype.metrics = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/status/metrics',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.storage = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/storage.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.threads = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/status/threads',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.options = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/options.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.storage_detail = function( pluginName, callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/storage/' + pluginName + '.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.storage_enable = function( pluginName, value, callback ){
+  value = value.toLowerCase();
+  if( value != "true" && value != "false" ){
+    callback(new Error( "the Value must be either true or false"));
+  } else {
+    var href = url.format({
+      protocol: this.protocol,
+      hostname: this.host,
+      pathname: '/storage/' + pluginName + '/enable/' + value,
+      port: this.port
+    })
+
+    var queryOptions = {
+      uri: href
+    }
+
+    request(queryOptions, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        callback(null, response, body);
+      }
+    });
+  }
+}
+
+Client.prototype.storage_update = function(plugin_name, config, callback) {
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: 'localhost',
+    pathname: '/storage/' + plugin_name + '.json',
+    port: 8047
+  })
+  var headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Accept':'application/json'
+  }
+  var queryOptions = {
+    uri: href,
+    method: 'POST',
+    headers: headers,
+    json: config
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log('victory')
+      callback(null, body);
+    }
+  });
+};
+
+Client.prototype.profiles = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/profiles.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.profile = function( queryID, callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/profiles/' + queryID + '.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.profile_cancel = function( queryID, callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/profiles/cancel/' + queryID + '.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
+
+Client.prototype.cluster = function( callback ){
+  var href = url.format({
+    protocol: this.protocol,
+    hostname: this.host,
+    pathname: '/cluster.json',
+    port: this.port
+  })
+
+  var queryOptions = {
+    uri: href
+  }
+
+  request(queryOptions, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, response, body);
+    }
+  });
+}
